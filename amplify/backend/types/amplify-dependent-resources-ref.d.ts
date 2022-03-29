3e8695c3edc78d84d8354874b0e59939cc5ab08a
Lucas Forbes <52499909+lucasforbes@users.noreply.github.com>
2022-03-28 22:46:39 -0600

    project file structure cleaned

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,40 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "autoabstract07a4de1bCustomMessage": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "autoabstract07a4de1bPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "S3Trigger20e43ea6": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "auth": {
+        "autoabstract07a4de1b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "storage": {
+        "s392ceb066": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
 	},
 	output: {
 		path: __dirname + '/public',
+		publicPath: "/views/public/",
 		filename: '[name].js',
 		chunkFilename: '[name].[id].js'
 	},
@@ -28,7 +29,10 @@ module.exports = {
 				use: {
 					loader: 'svelte-loader',
 					options: {
-						emitCss: true,
+						// Switch for production?
+						// emitCss: true,
+						// hotReload: true,
+						emitCss: false,
 						hotReload: true,
 						preprocess: sveltePreprocess({
 							// https://github.com/kaisermann/svelte-preprocess/#user-content-options
@@ -65,3 +69,89 @@ module.exports = {
 	],
 	devtool: prod ? false: 'source-map'
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const path = require('path');
+// const sveltePreprocess = require('svelte-preprocess');
+
+// const mode = process.env.NODE_ENV || 'development';
+// const prod = mode === 'production';
+
+// module.exports = {
+// 	entry: {
+// 		bundle: ['./src/main.js']
+// 	},
+// 	resolve: {
+// 		alias: {
+// 			svelte: path.resolve('node_modules', 'svelte')
+// 		},
+// 		extensions: ['.mjs', '.js', '.svelte'],
+// 		mainFields: ['svelte', 'browser', 'module', 'main']
+// 	},
+// 	output: {
+// 		path: __dirname + '/public',
+// 		filename: '[name].js',
+// 		chunkFilename: '[name].[id].js'
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.svelte$/,
+// 				use: {
+// 					loader: 'svelte-loader',
+// 					options: {
+// 						// emitCss: true,
+// 						// hotReload: true,
+// 						emitCss: false,
+// 						hotReload: false,
+// 						preprocess: sveltePreprocess({
+// 							// https://github.com/kaisermann/svelte-preprocess/#user-content-options
+// 							sourceMap: !prod,
+// 							postcss: {
+// 								plugins: [
+// 									require("tailwindcss"), 
+// 									// require("autoprefixer"),
+// 									require("postcss-nesting")
+// 								],
+// 							},
+// 						}),
+// 					}
+// 				}
+// 			},
+// 			{
+// 				test: /\.css$/,
+// 				use: [
+// 					/**
+// 					 * MiniCssExtractPlugin doesn't support HMR.
+// 					 * For developing, use 'style-loader' instead.
+// 					 * */
+// 					prod ? MiniCssExtractPlugin.loader : 'style-loader',
+// 					'css-loader'
+// 				]
+// 			}
+// 		]
+// 	},
+// 	mode,
+// 	plugins: [
+// 		new MiniCssExtractPlugin({
+// 			filename: '[name].css'
+// 		})
+// 	],
+// 	devtool: prod ? false: 'source-map'
+// };
